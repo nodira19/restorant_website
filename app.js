54cc1c44d32716a2687e765246f2f18b0d04a0de
Nodira_M <113989539+nodira19@users.noreply.github.com>
2022-10-08 12:19:15 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,90 @@
+let menu = document.querySelector('#menu-bars');
+let navbar= document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+let section = document.querySelector('section');
+let navLinks = document.querySelector('header .navbar a');
+
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+
+
+    section.forEach(sec =>{
+
+        let top = window.scrollY;
+        let height = sec.offsetHeight;
+        let offset = sec.offseTop -150;
+        let id = sec.getAttribute('id');
+
+        if(top => offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
+
+            });
+        };
+
+    });
+}
+
+document.querySelector('#search-icon').onclick = () =>{
+    document.querySelector('#search-form').classList.toggle('active');
+}
+
+document.querySelector('#close').onclick = () =>{
+    document.querySelector('#search-form').classList.remove('active');
+}
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+   loop:true,
+  });
+
+  var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+   loop:true,
+   breakpoints: {
+    0: {
+        slidePerView:1,
+    },
+    640: {
+        slidePerView: 2,
+    },
+    768: {
+        slidePerView: 2,
+    },
+    1024: {
+        slidePerView:3,
+    },
+   },
+  });
+
+  function loader(){
+    document.querySelector('.loader-container').classList.add('fade-out');
+  }
+
+  function fadeOut(){
+    setInterval(loader, 3000);
+  }
+
+  window.onload = fadeOut;
